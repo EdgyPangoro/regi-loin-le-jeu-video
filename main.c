ba188fc3f58c0ffe2ee4fd99139fb51bd37d15c8
Pangoro <32078167+EdgyPangoro@users.noreply.github.com>
2018-01-21 00:44:48 +0100

    premier commit :qlf:

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** test
+** oui
+** patate de fou
+*/
+
+#include <stdio.h>
+
+int	main()
+{
+	printf("stop hardbass regis\n");
+	printf("mentalo viens discord stp mon khey\n");
+	return (0);
+}
